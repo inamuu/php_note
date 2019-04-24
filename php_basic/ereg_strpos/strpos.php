51a8ab99a2f1a60442c99d3254c5bf7671cce3f1
inamuu <8310973+inamuu@users.noreply.github.com>
2019-04-24 11:43:51 +0900

    :+1: コメントを入れてわかりやすいように

--- php_basic/ereg_strpos/strpos.php
+++ php_basic/ereg_strpos/strpos.php
@@ -1,15 +1,33 @@
 <?php
-$var = 'minimum';
-if(strpos($var, 'minimum')) {
+
+/* 
+これだともちろんマッチしない
+*/
+$var1 = 'これはXXXXですね';
+if(strpos($var1, 'minimum')) {
     print "match\n";
 } else {
     print "unmatch\n";
 }
 
-$var2 = 'これはXXXXですね';
+/*
+これはマッチすることを期待するがマッチしない
+*/
+$var2 = 'minimum';
 if(strpos($var2, 'minimum')) {
     print "match\n";
 } else {
     print "unmatch\n";
 }
+
+/*
+マッチさせるために型もチェックするようにしないといけない
+*/
+$var3 = 'minimum';
+if(strpos($var3, 'minimum') !== false) {
+    print "match\n";
+} else {
+    print "unmatch\n";
+}
+
 ?>
