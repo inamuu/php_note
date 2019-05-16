a514ffe0a711ec98c3b71c7a0316a2ba9145155b
inamuu <8310973+inamuu@users.noreply.github.com>
2019-05-16 11:18:52 +0900

    :+1: zipをexecで

--- php_basic/zip/ziparchive.php
+++ php_basic/zip/ziparchive.php
@@ -3,11 +3,17 @@
 $file = './test.txt';
 $zipfile = './test.zip';
 
+// zipライブラリ
 $zip = new ZipArchive();
 $res = $zip->open($zipfile, ZipArchive::CREATE);
 if ($res === true){
     $zip->addFile($file);
     $zip->close();
 }
+
+// zipコマンド
+$zipDir = './hoge/';
+$zipFile = 'hoge.zip';
+$cmd = exec("zip -r " . "$zipFile" . " " . "$zipDir");
 
 ?>
